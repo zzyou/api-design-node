f6b92e4b859403d23de9e52753bbd91699179d5e
Zhenzhen You <swimreally@MacBook-Air.local>
2018-03-12 11:31:54 -0700

    app.get

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res) {
+    fs.readFile('index.html', function(err, buffer) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        var html = buffer.toString();
+        res.setHeader('Content-Type', 'text/html');
+        res.send(html);
+    })
+});
+
+// app.get('/', function(req, res) {
+//     // res.sendFile takes an absolute path to a file and 
+//     // sets the type based on the file name
+//     res.sendFile(__dirname + '/index.html', function(err) {
+//         if (err) {
+//             res.status(500).send(err);
+//         }
+//     })
+// });
+
+app.get('/data', function(req, res) {
+    res.json(jsonData);
+})
+
+var port = 3000;
+app.listen(port, function() {
+    console.log('listening on http://localhost:', port);
+});
