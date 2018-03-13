a24bfc2f84e4814b233b062e9a1137a3dfa186c1
Zhenzhen You <swimreally@MacBook-Air.local>
2018-03-12 17:03:43 -0700

    my own morgan

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 // TODO: mount the tigers route with a a new router just for tigers
 // exactly like lions below
+
+// "use strict"; 
+// // partial support of let and const.
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -13,6 +17,13 @@ app.use(morgan('dev'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+// app.use(function(req, res, next) {
+//   console.log('The body', req.body);
+//   next();
+// });
+// // my own morgan.
+
+
 // this is called mounting. when ever a req comes in for
 // '/lion' we want to use this router
 app.use('/lions', lionRouter);
